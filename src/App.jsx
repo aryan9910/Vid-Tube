2a39af20947cc9bfc8bb0f21ac63c9bb272b7c04
aryan9910 <aryanj9910@gmail.com>
2025-06-15 11:15:54 +0530

    Fix import path casing for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from './Pages/Home/Home';
-import Video from './Pages/video/Video';
+import Video from './Pages/Video/Video';
 const App = () => {
 
   const [sidebar, setSidebar] = useState(true) 
